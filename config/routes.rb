f0a5a25c5fe6199bb04cf715a9442aa5cf7a5db6
tuba0809 <tubasa8342849@yahoo.co.jp>
2021-09-04 17:10:31 +0000

    Day1

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update] do
+      resources :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
  end
   resources :books do
   resource :favorites, only: [:create, :destroy]
